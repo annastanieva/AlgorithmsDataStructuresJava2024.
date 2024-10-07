a061d9aafb9e2566ab6a6796ddf87682cc1e82be
annastanieva <anna.stanieva@stud.onu.edu.ua>
2024-10-07 22:27:15 +0300

    Update ListNode.java

--- LinkedList/ListNode.java
+++ LinkedList/ListNode.java
@@ -1,3 +1,27 @@
 public class ListNode {
     int val;
     ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        ListNode current = this;
+        while (current != null) {
+            result.append(current.val).append(" -> ");
+            current = current.next;
+        }
+        result.append("null");
+        return result.toString();
+    }
+}
