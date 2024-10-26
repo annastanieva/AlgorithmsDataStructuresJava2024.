762448ed2ebf897d56c09bd2e9c91dc1ff9b502e
annastanieva <anna.stanieva@stud.onu.edu.ua>
2024-10-26 13:25:00 +0300

    Update Main.java

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -4,8 +4,8 @@ public class Main {
         Solution solution = new Solution();
 
         // Приклад 1: list1 = [1, 2, 4], list2 = [1, 3, 4]
-        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4, null)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4, null)));
         ListNode mergedList1 = solution.mergeTwoLists(list1, list2);
         System.out.println(mergedList1);
 
@@ -17,7 +17,7 @@ public class Main {
 
         // Приклад 3: list1 = [], list2 = [0]
         ListNode list5 = null;
-        ListNode list6 = new ListNode(0);
+        ListNode list6 = new ListNode(0, null);
         ListNode mergedList3 = solution.mergeTwoLists(list5, list6);
         System.out.println(mergedList3);
     }
